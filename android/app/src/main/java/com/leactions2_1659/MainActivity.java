72454974f1c4b20118e8650533870d4a2268df20
Crowdbotics <team@crowdbotics.com>
2020-07-09 14:55:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/leactions2_1659/MainActivity.java
@@ -0,0 +1,15 @@
+package com.leactions2_1659;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "leactions2_1659";
+    }
+}
